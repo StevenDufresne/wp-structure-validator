e18c8053cf30fd5e92af92f78a0797b1e8341484
dufresnesteven <steve.dufresne@automattic.com>
2020-09-14 15:00:27 +0900

    Try looking for files.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const io = require('@actions/io');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+    const awaitStyle = io.exists('style.css');
+
+    console.log(awaitStyle);
+    
+    const awaitNonExists = io.exists('nomatch.css');
+
+    console.log( awaitNonExists);
+
 } catch (error) {
-  core.setFailed(error.message);
+    console.log(error);
 }
