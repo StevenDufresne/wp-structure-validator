26898ae61d3e7ba8a95cd39b5dce4ebb85217e51
dufresnesteven <steve.dufresne@automattic.com>
2020-09-15 15:26:41 +0900

    Let's go.

--- goodbye.sh
+++ goodbye.sh
@@ -1,1 +1,8 @@
-echo "Goodbye"
+
+##zip theme.zip -r ./ -x '.*'
+
+# # Turn it on
+npm run wp-env start
+
+# Trigger the check
+npm run wp-env run cli eval-file ./config/test.php
--- index.sh
+++
@@ -1,8 +0,0 @@
-
-##zip theme.zip -r ./ -x '.*'
-
-# # Turn it on
-npm run wp-env start
-
-# Trigger the check
-npm run wp-env run cli eval-file ./config/test.php
