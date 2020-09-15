88f4f46440a55a142c9059ee65c6d111812c7b5f
dufresnesteven <steve.dufresne@automattic.com>
2020-09-15 14:51:09 +0900

    Skip zip.

--- index.sh
+++ index.sh
@@ -1,8 +1,8 @@
 
-zip theme.zip -r ./ -x '.*'
+##zip theme.zip -r ./ -x '.*'
 
 # # Turn it on
 npm run wp-env start
 
-# # Trigger the check
-# npm run wp-env run cli eval-file ./config/test.php
+# Trigger the check
+npm run wp-env run cli eval-file ./config/test.php
