4444d6d0c8cd53e23cfb46736602893ef73b588e
dufresnesteven <steve.dufresne@automattic.com>
2020-09-16 12:21:05 +0900

    Check the context of the github object.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 const start = async () => {
-    const folder = await exec.exec('ls');
-    console.log( folder );
-    const resp = await exec.exec('npm run wp-env start');
-    console.log( resp );
+	console.log(JSON.stringify(github));
+	const folder = await exec.exec('ls');
+	console.log(folder);
+	const resp = await exec.exec('npm run wp-env start');
+	console.log(resp);
 };
 
 start();
