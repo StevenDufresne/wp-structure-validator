677fdf19f33de6a03c64cb3032a394a22386c4fd
dufresnesteven <steve.dufresne@automattic.com>
2020-09-16 10:15:51 +0900

    Remove the other calls.

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 ls
 
 # # Turn it on
-npm run wp-env start
+# npm run wp-env start
 
-# Trigger the check
-npm run wp-env run cli eval-file ./config/test.php
+# # Trigger the check
+# npm run wp-env run cli eval-file ./config/test.php
