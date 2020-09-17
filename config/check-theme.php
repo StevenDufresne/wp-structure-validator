1ea06ba58220c429b6acf0b5528f9ea509fd6862
dufresnesteven <steve.dufresne@automattic.com>
2020-09-17 09:27:36 +0900

    Try new checks.

--- config/check-theme.php
+++ config/check-theme.php
@@ -59,11 +59,10 @@ class WPORG_Themes_Test {
 		$passes = $this->check_theme( $theme_files );
 		var_dump( $passes );
 
-		exit(  $passes ) );
 		if( ! $passes ) {
-			//echo "::set-output name=results::" . display_themechecks();
-			//echo "::error::" . display_themechecks();
-			exit(0);
+			echo "::set-output name=results::" . display_themechecks();
+			echo "::error::Failed check";
+			exit("Failed check");
 		}
 
 		exit(1);
