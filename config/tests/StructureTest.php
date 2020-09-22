1f4719e5133c2c890654e1d5480f9aa2b2fe170f
dufresnesteven <steve.dufresne@automattic.com>
2020-09-22 13:22:45 +0900

    Try php unit tests.

---
+++ config/tests/StructureTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class StructureTest extends TestCase
+{
+    public function testsThatStyleIsPresent(): void
+    {
+        $this->assertFileExists( '/test-theme/style.css', 'We require you have a style.css file.' );
+    }
+
+    public function testsThatIndexIsPresent(): void
+    {
+        $this->assertFileExists( '/test-theme/index.php', 'We require you have a index.php file.' );
+    }
+
+    public function testsThatCommentsIsPresent(): void
+    {
+        $this->assertFileExists( '/test-theme/comments.php', 'We require you have a comments.php file.' );
+    }
+
+    public function testsThatScreenshotIsPresent(): void
+    {
+        $this->assertFileExists( '/test-theme/screenshot.png', 'We require you have a screenshot.png file.' );
+    }
+}
