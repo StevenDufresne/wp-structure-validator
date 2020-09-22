95925397ba39c08cb69df8fa1daef10052182d0e
dufresnesteven <steve.dufresne@automattic.com>
2020-09-22 14:45:18 +0900

    Fix the file.

--- config/tests/StructureTest.php
+++ config/tests/StructureTest.php
@@ -4,26 +4,21 @@ final class StructureTest extends TestCase
 {
     public function testsThatStyleIsPresent(): void
     {
-        $h = new ThemeHelper();           
-        $f = $h->get_all_files( './test-theme' );
-
-        var_dump( $f );
-
-        $this->assertFileExists( '/test-theme/style.css', 'We require you have a style.css file.' );
+        $this->assertFileExists( './test-theme/style.css', 'We require you have a style.css file.' );
     }
 
     public function testsThatIndexIsPresent(): void
     {
-        $this->assertFileExists( '/test-theme/index.php', 'We require you have a index.php file.' );
+        $this->assertFileExists( './test-theme/index.php', 'We require you have a index.php file.' );
     }
 
     public function testsThatCommentsIsPresent(): void
     {
-        $this->assertFileExists( '/test-theme/comments.php', 'We require you have a comments.php file.' );
+        $this->assertFileExists( './test-theme/comments.php', 'We require you have a comments.php file.' );
     }
 
     public function testsThatScreenshotIsPresent(): void
     {
-        $this->assertFileExists( '/test-theme/screenshot.png', 'We require you have a screenshot.png file.' );
+        $this->assertFileExists( './test-theme/screenshot.png', 'We require you have a screenshot.png file.' );
     }
 }
