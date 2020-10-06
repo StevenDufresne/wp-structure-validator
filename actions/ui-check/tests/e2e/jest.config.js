548723197568076109e2c3b67a95cede7ec9221f
dufresnesteven <steve.dufresne@automattic.com>
2020-10-06 11:11:17 +0900

    Add in accessibility checks.

--- actions/ui-check/tests/e2e/jest.config.js
+++ actions/ui-check/tests/e2e/jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
+	testTimeout: 10000,
 	setupFilesAfterEnv: [
 		'@wordpress/jest-console',
+		'@wordpress/jest-puppeteer-axe',
 		'expect-puppeteer',
 	],
 };
