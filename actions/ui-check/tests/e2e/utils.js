6c9301c4ce26ec83e2309dbf1b479900b83911aa
dufresnesteven <steve.dufresne@automattic.com>
2020-10-07 14:40:00 +0900

    Add some doc blocks.

--- actions/ui-check/tests/e2e/utils.js
+++ actions/ui-check/tests/e2e/utils.js
@@ -1,9 +1,22 @@
 const core = require( '@actions/core' );
 
+/**
+ * Removes some noise that exists in the testing framework error messages.
+ *
+ * @param {string} msg Error message thrown by testing framework.
+ * @returns string
+ */
 export const cleanErrorMessage = ( msg ) => {
 	return msg.replace( 'expect(received).toPassAxeTests(expected)', '' );
 };
 
+/**
+ * Returns a formatted version of the url.
+ *
+ * @param {string} path Url of the page.
+ * @param {string} query The query string of the url.
+ * @returns string
+ */
 export const getDefaultUrl = ( path, query ) => {
 	let defaultUrl = path;
 
@@ -14,6 +27,12 @@ export const getDefaultUrl = ( path, query ) => {
 	return `(${ defaultUrl })`;
 };
 
+/**
+ * Prints a message
+ *
+ * @param {command} command The name of the command that matches the `core` library messaging commands.
+ * @param {array} lines The content to print.
+ */
 export const printMessage = ( command, lines ) => {
 	// Github actions should automatically set CI
 	if ( ! process.env.CI ) {
