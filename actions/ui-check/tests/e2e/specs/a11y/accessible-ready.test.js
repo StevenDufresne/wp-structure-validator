3a6a4cc5ba3151cdeab78cde859bef22fea759a8
dufresnesteven <steve.dufresne@automattic.com>
2020-10-08 14:57:19 +0900

    Uncomment stuffs.

--- actions/ui-check/tests/e2e/specs/a11y/accessible-ready.test.js
+++ actions/ui-check/tests/e2e/specs/a11y/accessible-ready.test.js
@@ -11,32 +11,32 @@ import urls from './pages';
 import { cleanErrorMessage, getDefaultUrl, printMessage } from '../../utils';
 
 describe( 'Accessible: Tag Ready', () => {
-	// test.each( urls )(
-	// 	'Must pass Axe tests on %s',
-	// 	async ( name, path, query ) => {
-	// 		await page.goto( createURL( path, query ) );
+	test.each( urls )(
+		'Must pass Axe tests on %s',
+		async ( name, path, query ) => {
+			await page.goto( createURL( path, query ) );
 
-	// 		try {
-	// 			await expect( page ).toPassAxeTests( {
-	// 				options: {
-	// 					runOnly: {
-	// 						type: 'tag',
-	// 						values: [ 'wcag2a' ],
-	// 					},
-    //                 },
-    //                 exclude: [ [ '.entry-content' ] ],
-	// 			} );
-	// 		} catch ( e ) {
-	// 			printMessage( 'setFailed', [
-	// 				'[ Accessibility - Required Tests ]:',
-	// 				`Running tests on ${ name } ${ getDefaultUrl(
-	// 					path,
-	// 					query
-	// 				) } using: \nhttps://github.com/wpaccessibility/a11y-theme-unit-test`,
-	// 				cleanErrorMessage( e.message ),
-	// 			] );
-	// 			throw Error();
-	// 		}
-	// 	}
-	// );
+			try {
+				await expect( page ).toPassAxeTests( {
+					options: {
+						runOnly: {
+							type: 'tag',
+							values: [ 'wcag2a' ],
+						},
+                    },
+                    exclude: [ [ '.entry-content' ] ],
+				} );
+			} catch ( e ) {
+				printMessage( 'setFailed', [
+					'[ Accessibility - Required Tests ]:',
+					`Running tests on ${ name } ${ getDefaultUrl(
+						path,
+						query
+					) } using: \nhttps://github.com/wpaccessibility/a11y-theme-unit-test`,
+					cleanErrorMessage( e.message ),
+				] );
+				throw Error();
+			}
+		}
+	);
 } );
--- actions/ui-check/tests/e2e/specs/a11y/optional.test.js
+++ actions/ui-check/tests/e2e/specs/a11y/optional.test.js
@@ -10,31 +10,31 @@ import urls from './pages';
 import { cleanErrorMessage, getDefaultUrl, printMessage } from '../../utils';
 
 describe( 'Accessibility: Best Practices', () => {
-	// test.each( urls )(
-	// 	'Should pass Best Practice Axe tests on %s',
-	// 	async ( name, path, query ) => {
-	// 		await page.goto( createURL( path, query ) );
+	test.each( urls )(
+		'Should pass Best Practice Axe tests on %s',
+		async ( name, path, query ) => {
+			await page.goto( createURL( path, query ) );
 
-	// 		try {
-	// 			await expect( page ).toPassAxeTests( {
-	// 				options: {
-	// 					runOnly: {
-	// 						type: 'tag',
-	// 						values: [ 'best-practice' ],
-	// 					},
-	// 					exclude: [ [ '.entry-content' ] ],
-	// 				},
-	// 			} );
-	// 		} catch ( e ) {
-	// 			printMessage( 'warning', [
-	// 				'[ Accessibility - Best Practice Tests ]:',
-	// 				`Running tests on ${ name } ${ getDefaultUrl(
-	// 					path,
-	// 					query
-	// 				) } using: \nhttps://github.com/wpaccessibility/a11y-theme-unit-test`,
-	// 				cleanErrorMessage( e.message ),
-	// 			] );
-	// 		}
-	// 	}
-	// );
+			try {
+				await expect( page ).toPassAxeTests( {
+					options: {
+						runOnly: {
+							type: 'tag',
+							values: [ 'best-practice' ],
+						},
+						exclude: [ [ '.entry-content' ] ],
+					},
+				} );
+			} catch ( e ) {
+				printMessage( 'warning', [
+					'[ Accessibility - Best Practice Tests ]:',
+					`Running tests on ${ name } ${ getDefaultUrl(
+						path,
+						query
+					) } using: \nhttps://github.com/wpaccessibility/a11y-theme-unit-test`,
+					cleanErrorMessage( e.message ),
+				] );
+			}
+		}
+	);
 } );
