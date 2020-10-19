828d2bfd2d06e5aa60c89d0fb8c821b1691e5ee1
dufresnesteven <steve.dufresne@automattic.com>
2020-10-19 11:55:28 +0900

    Format files.

--- actions/ui-check/tests/e2e/jest.config.js
+++ actions/ui-check/tests/e2e/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
 	testTimeout: 1000000,
 	setupFilesAfterEnv: [
-        '<rootDir>/jest.defaults.js',
+		'<rootDir>/jest.defaults.js',
 		'@wordpress/jest-console',
 		'@wordpress/jest-puppeteer-axe',
 		'expect-puppeteer',
