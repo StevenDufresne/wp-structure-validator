5489402ebec007b9eea43c3eb592ed54868e1e5e
dufresnesteven <steve.dufresne@automattic.com>
2020-11-05 09:51:24 +0900

    Add in a check for a child theme and format tests.

--- actions/structure-check/tests/StructureTest.php
+++ actions/structure-check/tests/StructureTest.php
@@ -2,23 +2,33 @@
 use PHPUnit\Framework\TestCase;
 final class StructureTest extends TestCase
 {
-    const REL_THEME_LOCATION = '../../test-theme';
+	const REL_THEME_LOCATION = '../../test-theme';
 
-    public function testsThatStyleIsPresent(): void
-    {
-        $this->assertFileExists( self::REL_THEME_LOCATION . '/style.css', '::error:: We require you have a style.css file.' );
-    }
+	function isChildTheme(): bool {
+		$css_contents = file_get_contents( self::REL_THEME_LOCATION . '/style.css');
 
-    public function testsThatIndexIsPresent(): void
-    {
-        $this->assertFileExists( self::REL_THEME_LOCATION . '/index.php', '::error::We require you have an index.php file.' );
-    }
+		return preg_match( '/[ \t\/*#]*Template:/i', $css_contents ) === 1;
+	}
 
-    public function testsThatScreenshotIsPresent(): void
-    {
-        $hasPNG = file_exists( self::REL_THEME_LOCATION . '/screenshot.png' );
-        $hasJPG = file_exists( self::REL_THEME_LOCATION . '/screenshot.jpg' ) || file_exists( self::REL_THEME_LOCATION . '/screenshot.jpeg' );
+	public function testsThatStyleIsPresent(): void
+	{
+		$this->assertFileExists( self::REL_THEME_LOCATION . '/style.css', '::error:: We require you have a style.css file.' );
+	}
 
-        $this->assertTrue( $hasPNG || $hasJPG, '::error::We require you have a screenshot.png or screenshot.jpg file.' );
-    }
+	public function testsThatIndexIsPresent(): void
+	{
+		if( self::isChildTheme() ) {
+			$this->markTestSkipped('Index.php is not required for a child theme.');
+		}
+
+		$this->assertFileExists( self::REL_THEME_LOCATION . '/index.php', '::error::We require you have an index.php file.' );
+	}
+
+	public function testsThatScreenshotIsPresent(): void
+	{
+		$hasPNG = file_exists( self::REL_THEME_LOCATION . '/screenshot.png' );
+		$hasJPG = file_exists( self::REL_THEME_LOCATION . '/screenshot.jpg' ) || file_exists( self::REL_THEME_LOCATION . '/screenshot.jpeg' );
+
+		$this->assertTrue( $hasPNG || $hasJPG, '::error::We require you have a screenshot.png or screenshot.jpg file.' );
+	}
 }
